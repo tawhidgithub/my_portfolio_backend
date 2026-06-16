4b63dbd7e4e1d6525523778572f6054b3e669d64
Tawhid <titawhid02@gmail.com>
2026-06-16 21:40:34 +0600

    attempt to fix the 404 3

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API is running"));
 
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`);
